4075d2f63831dc22c9b7576f9cf5208305b74fb2
afterloe <lm6289511@gmail.com>
2018-04-06 19:21:36 +0800

    feat: spring cloud 项目构建

--- src/main/kotlin/cn/awpaas/Launch.kt
+++ src/main/kotlin/cn/awpaas/Launch.kt
@@ -4,10 +4,12 @@ import de.codecentric.boot.admin.config.EnableAdminServer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.web.reactive.config.EnableWebFlux
 
-@SpringBootApplication
-@EnableAdminServer
 @EnableDiscoveryClient
+@EnableAdminServer
+@EnableWebFlux
+@SpringBootApplication
 class Launch
 
 fun main(args: Array<String>) {
